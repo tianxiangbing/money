e4479c07b2c5d04979d332c450719a1046679699
tianxiangbing <55342775@qq.com>
2019-12-24 09:11:44 +0800

    update

--- src/pages/list/index.jsx
+++ src/pages/list/index.jsx
@@ -1,199 +1,279 @@
 import React from 'react';
-import {Jsonp} from 'utils/curry';
+import { Jsonp } from 'utils/curry';
 import './index.less';
 import Item from './item';
-import {InputContainer,Input} from 'jsx-input';
-const NumberInput = InputContainer(Input,/^\d*$/);
-export default class List extends React.Component{
+import { InputContainer, Input } from 'jsx-input';
+const NumberInput = InputContainer(Input, /[\d][a-zA-Z]*/);
+export default class List extends React.Component {
     sortBy = 1;
-    hasData=false;
-    constructor(props){
+    hasData = false;
+    constructor(props) {
         super(props);
-        let yestoday = JSON.parse(localStorage.getItem('yestoday') ||"{}")||{};
-        if(yestoday.date ){
+        let yestoday = JSON.parse(localStorage.getItem('yestoday') || "{}") || {};
+        if (yestoday.date) {
             let now = new Date();
             let d = new Date(yestoday.date);
             // if(now.getDate() - d.getDate()==1){
-                // yestoday={};
-                this.hasData = true;
+            // yestoday={};
+            this.hasData = true;
             // }
         }
-        this.state={
-            date:yestoday.date|| +new Date(),
-            data:yestoday.data||[],
-            owner:yestoday.owner||[],
-            showdata:true,
-            code:''
+        this.state = {
+            date: yestoday.date || +new Date(),
+            data: yestoday.data || [],
+            owner: yestoday.owner || [],
+            showdata: true,
+            code: '',
+            selected: []
         }
     }
-    componentDidMount(){
-        if(!this.hasData || this.state.data.length===0 ) this.request();
+    componentDidMount() {
+        if (!this.hasData || this.state.data.length === 0) this.request();
     }
-    request(){
+    request() {
         let _this = this;
-        Jsonp('//nuyd.eastmoney.com/EM_UBG_PositionChangesInterface/api/js?dtformat=HH:mm:ss&js=({data:[(x)],pc:(pc)})&rows=64&page=1&type=8213',{
-            param:'cb'
-        },function(res){
-            console.log(res);
+        Jsonp('http://nuyd.eastmoney.com/EM_UBG_PositionChangesInterface/api/js?dtformat=HH:mm:ss&js=({data:[(x)],pc:(pc)})&rows=64&page=1&type=8213', {
+            param: 'cb'
+        }, function (res) {
+            // console.log(res);
             _this.setState({
-                data:res.data.map(item=>{
+                data: res.data.map(item => {
                     let arr = item.split(',');
-                    let code  = arr[4];
+                    let code = arr[4];
                     let name = arr[0];
                     let info = arr[2];
                     return {
-                        code,name,info
+                        code, name, info
                     }
-                }).filter(item=>!/^300*/.test(item.code))
-            },()=>{
+                }).filter(item => !/^300*/.test(item.code))
+            }, () => {
                 _this.save();
             })
         });
     }
-    addHandle=()=>{
+    addHandle = () => {
         let d = this.state.owner;
-        d.push({code:this.state.code})
-        this.setState({owner:d},()=>{
+        d.push({ code: this.state.code })
+        this.setState({ owner: d }, () => {
             this.save();
         })
     }
-    save(){
-        localStorage.setItem('yestoday',JSON.stringify(this.state));
+    save() {
+        localStorage.setItem('yestoday', JSON.stringify(this.state));
     }
-    sortData(col,type){
+    sortData(col, type) {
         let data = this.state[type];
         this.sortBy = this.sortBy * -1;
-        this.setState({[type]:data.sort((a,b)=>{
-            return (a[col]-b[col])*this.sortBy;
-        })})
+        this.setState({
+            [type]: data.sort((a, b) => {
+                return (a[col] - b[col]) * this.sortBy;
+            })
+        })
     }
-    onUpdate(index,dataType,data){
-        if(dataType === 'owner'){
+    onUpdate(index, dataType, data) {
+        if (dataType === 'owner') {
             let owner = this.state.owner;
-            Object.assign(owner[index],data); 
-            this.setState({owner:owner});
-        }else{
+            Object.assign(owner[index], data);
+            this.setState({ owner: owner });
+        } else {
             let sd = this.state.data;
-            Object.assign(sd[index],data); 
-            this.setState({data:sd});
-        }
-    }
-    onFan(t){
-        if(t===1){
-            //根据量比和换手率来排序
-            //换手为5 * 0.3，量比为3  * 1
-            let arr = ['owner','data'];
-            arr.forEach(type=>{
-                let data = this.state[type];
-                this.setState({[type]:data.sort((a,b)=>{
-                    let lb11 = this.compute(5,20,a["lb"])
-                    let lb22 = this.compute(5,20,b["lb"])
-                    let hs1 = this.compute(5,10,a['hsl'])
-                    let hs2 = this.compute(5,10,b['hsl'])
-                    let n1 = lb11 + hs1
-                    let n2 = lb22 + hs2;
-                    if(isNaN(n1))n1=0;
-                    if(isNaN(n2))n2=0;
-                    return n2-n1;
-                })})
-            })
-        }
-    }
-    compute(x,j,v){
-        let lb1 = Math.min(Math.max(0,v),x*2);
-        return Math.tan(j) * 5- Math.abs(5-lb1);
-    }
-    onClear = ()=>{
-        this.request();  
-    }
-    hide(type){
-        if(type==1){
-            this.setState({showdata:!this.state.showdata});
-        }
-    }
-    findIndex(arr,callback) { 
-        for (var i = 0; i < arr.length; i++) { 
-            if (callback (arr[i]) ) return i; 
-        } 
-        return -1; 
-    }
-    remove = function(arr,callback) { 
-        var index = this.findIndex(arr,callback); 
-        if (index > -1) { 
-            arr.splice(index, 1); 
-        } 
-    }
-    action =(source,type,code)=>{
-        let data = this.state[source];
-        if(type==='del'){
-            this.remove(data,(d)=>{
-                return d.code ===code;
-            })
-            this.setState({[source]:data},()=>{
-                this.save();
-            });
-        }
-        if(type==='addowner'){
-            data = this.state.owner;
-            data=[{code}].concat(data)
-            this.setState({owner:data},()=>{
-                this.save();
-            });
-        }
-        if(type==='delowner'){
-            data = this.state.owner;
-            this.remove(data,(d)=>{
-                return d.code ===code;
-            })
-            this.setState({owner:data},()=>{
-                this.save();
-            });
-        }
-    }
-    onChangeHandle =(v)=>{
-        this.setState({code:v})
-    }
-    render(){
-        return (
-            <div>
-                <div><NumberInput onChange={this.onChangeHandle} placeholder="输入股票代码"/><button onClick={this.addHandle}>新增监听</button></div>
-                <p>数据来源于前一日突破60日的均线</p>
-                <div><button className="btn" onClick={this.onFan.bind(this,1)}>方案一</button></div>
-                <div><button className="btn" onClick={this.onClear}>清除历史</button></div>
-                <h2>异动股行情<button onClick={this.hide.bind(this,1)}>隐藏/显示</button></h2>
-                <div className="half">
-                {this.state.showdata ?<table className="list">
-                    <thead>
-                        <tr>
-                            <td width="30">序号</td>
-                            <td>代码</td>
-                            <td>名称</td>
-                            <td>信息</td>
-                            <td onClick={this.sortData.bind(this,'zf','data')}>涨幅</td>
-                            <td>价格</td>
-                            <td>今开</td>
-                            <td>昨收</td>
-                            <td>最高</td>
-                            <td>最低</td>
-                            <td onClick={this.sortData.bind(this,'ltsz','data')} >流通</td>
-                            <td onClick={this.sortData.bind(this,'hsl','data')}>换手率</td>
-                            <td onClick={this.sortData.bind(this,'sy','data')}>市盈</td>
-                            <td onClick={this.sortData.bind(this,'lb','data')}>量比</td>
-                            <td>操作</td>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    {
-                        this.state.data.map((item,index)=>{
-                            let hasOwner = this.state.owner.filter(it=>it.code===item.code).length;
-                            return (
-                                <Item action={this.action.bind(this,'data')} hasOwner={hasOwner} key={item.code} index={index} data={item} onUpdate={this.onUpdate.bind(this,index,'data')}/>
-                            )
+            Object.assign(sd[index], data);
+            this.setState({ data: sd }, () => {
+                //统计板块信息
+                // console.log('state:::::::::::', this.state)
+                let bkArr = {};
+                this.state.data.forEach(item => {
+                    if (item.bk) {
+                        let bk = item.bk.split(';');
+                        // bkArr.concat(bk);
+                        bk.forEach(it => {
+                            let tmp = bkArr[it]
+                            if (tmp) {
+                                bkArr[it]++;
+                            } else {
+                                bkArr[it] = 1;
+                            }
                         })
                     }
-                    </tbody>
-                </table>:undefined
+                })
+                this.setState({ bkList: bkArr });
+            });
+        }
+    }
+    onFan(t) {
+        if (t === 1) {
+            //根据量比和换手率来排序
+            //换手为5 * 0.3，量比为3  * 1
+            let arr = ['owner', 'data'];
+            arr.forEach(type => {
+                let data = this.state[type];
+                this.setState({
+                    [type]: data.sort((a, b) => {
+                        let lb11 = this.compute(3, 20, a["lb"])
+                        let lb22 = this.compute(3, 20, b["lb"])
+                        let hs1 = this.compute(3, 15, a['hsl'])
+                        let hs2 = this.compute(3, 15, b['hsl'])
+                        let n1 = lb11 + hs1
+                        let n2 = lb22 + hs2;
+                        if (isNaN(n1)) n1 = 0;
+                        if (isNaN(n2)) n2 = 0;
+                        //市盈利
+                        let sy1 = this.compute(40, 45, isNaN(a["sy"]) ? 100 : a["sy"]);
+                        let sy2 = this.compute(40, 45, isNaN(b["sy"]) ? 100 : b["sy"]);
+                        n1 = n1 + sy1 / 12;
+                        n2 = n2 + sy2 / 12;
+                        // console.log(n2,n1)
+                        return n2 - n1;
+                    })
+                })
+            })
+        }
+    }
+    compute(x, j, v) {
+        let lb1 = Math.min(Math.max(0, v), x * 2);
+        return Math.tan(j) * 5 - Math.abs(5 - lb1);
+    }
+    onClear = () => {
+        this.request();
+    }
+    hide(type) {
+        if (type == 1) {
+            this.setState({ showdata: !this.state.showdata });
+        }
+    }
+    findIndex(arr, callback) {
+        for (var i = 0; i < arr.length; i++) {
+            if (callback(arr[i])) return i;
+        }
+        return -1;
+    }
+    remove = function (arr, callback) {
+        var index = this.findIndex(arr, callback);
+        if (index > -1) {
+            arr.splice(index, 1);
+        }
+    }
+    action = (source, type, code) => {
+        let data = this.state[source];
+        if (type === 'del') {
+            this.remove(data, (d) => {
+                return d.code === code;
+            })
+            this.setState({ [source]: data }, () => {
+                this.save();
+            });
+        }
+        if (type === 'addowner') {
+            data = this.state.owner;
+            data = [{ code }].concat(data)
+            this.setState({ owner: data }, () => {
+                this.save();
+            });
+        }
+        if (type === 'delowner') {
+            data = this.state.owner;
+            this.remove(data, (d) => {
+                return d.code === code;
+            })
+            this.setState({ owner: data }, () => {
+                this.save();
+            });
+        }
+    }
+    onChangeHandle = (v) => {
+        this.setState({ code: v })
+    }
+    renderBk = () => {
+        let list = [];
+        for (let k in this.state.bkList) {
+            let item = this.state.bkList[k];
+            list.push({ name: k, num: item });
+        }
+        return list.sort((a, b) => {
+            return b.num - a.num;
+        }).map(item => <span key={item.name} className={this.state.selected.indexOf(item.name) > -1 ? 'selected' : ''} onClick={this.onSelected.bind(this, item.name)}> {item.name}({item.num}) </span>)
+    }
+    onSelected(name) {
+        let index = this.state.selected.indexOf(name);
+        if (index > -1) {
+            this.state.selected.splice(index, 1);
+        } else {
+            this.state.selected.push(name)
+        }
+        this.setState({});
+    }
+    checkBk = (item) => {
+        if (this.state.selected.length == 0) {
+            return true;
+        } else {
+            let isfilter = false;
+            this.state.selected.forEach(it => {
+                if (item.bk && item.bk.split(';').indexOf(it) > -1) {
+                    isfilter = true;
                 }
+            })
+            return isfilter;
+        }
+    }
+    renderRow = () => {
+        let obj = {};
+        let arr = this.state.data.filter(item => {
+            //先去重
+            if (obj[item.code]) {
+                return false;
+            }
+            obj[item.code] = true;
+            return this.checkBk(item)
+        }).map((item, index) => {
+            // console.log('00000000000000::::::::',index)
+            let hasOwner = this.state.owner.filter(it => it.code === item.code).length;
+            return (
+                <Item action={this.action.bind(this, 'data')} hasOwner={hasOwner} key={item.code} index={index} data={item} onUpdate={this.onUpdate.bind(this, index, 'data')} />
+            )
+        })
+        return arr;
+    }
+    render() {
+        return (
+            <div>
+                <div>
+                    <NumberInput onChange={this.onChangeHandle} placeholder="输入股票代码" />
+                    <button onClick={this.addHandle}>新增监听</button>
+                </div>
+                <p>数据来源于前一日突破60日的均线</p>
+                <div><button className="btn" onClick={this.onFan.bind(this, 1)}>优选排序</button></div>
+                <div><button className="btn" onClick={this.onClear}>清除历史</button></div>
+                <h2>异动股行情<button onClick={this.hide.bind(this, 1)}>隐藏/显示</button></h2>
+                <div className="half">
+                    {this.state.showdata ? <div>
+                        <div className="bkList">板块：{this.renderBk()}</div>
+                        <table className="list">
+                            <thead>
+                                <tr>
+                                    <td width="30">序号</td>
+                                    <td>代码</td>
+                                    <td>名称</td>
+                                    <td>信息</td>
+                                    <td onClick={this.sortData.bind(this, 'zf', 'data')}>涨幅</td>
+                                    <td>价格</td>
+                                    <td>今开</td>
+                                    <td>昨收</td>
+                                    <td>最高</td>
+                                    <td>最低</td>
+                                    <td onClick={this.sortData.bind(this, 'ltsz', 'data')} >流通</td>
+                                    <td onClick={this.sortData.bind(this, 'hsl', 'data')}>换手率</td>
+                                    <td onClick={this.sortData.bind(this, 'sy', 'data')}>市盈</td>
+                                    <td onClick={this.sortData.bind(this, 'lb', 'data')}>量比</td>
+                                    <td>板块</td>
+                                    <td>操作</td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {this.renderRow()}
+                            </tbody>
+                        </table>
+                    </div> : undefined
+                    }
                 </div>
                 <h2>自选股行情</h2>
                 <table className="list">
@@ -203,27 +283,28 @@ export default class List extends React.Component{
                             <td>代码</td>
                             <td>名称</td>
                             <td>信息</td>
-                            <td onClick={this.sortData.bind(this,'zf','owner')}>涨幅</td>
+                            <td onClick={this.sortData.bind(this, 'zf', 'owner')}>涨幅</td>
                             <td>价格</td>
                             <td>今开</td>
                             <td>昨收</td>
                             <td>最高</td>
                             <td>最低</td>
-                            <td onClick={this.sortData.bind(this,'ltsz','data')} >流通</td>
-                            <td onClick={this.sortData.bind(this,'hsl','owner')}>换手率</td>
-                            <td onClick={this.sortData.bind(this,'sy','owner')}>市盈</td>
-                            <td onClick={this.sortData.bind(this,'lb','owner')}>量比</td>
+                            <td onClick={this.sortData.bind(this, 'ltsz', 'data')} >流通</td>
+                            <td onClick={this.sortData.bind(this, 'hsl', 'owner')}>换手率</td>
+                            <td onClick={this.sortData.bind(this, 'sy', 'owner')}>市盈</td>
+                            <td onClick={this.sortData.bind(this, 'lb', 'owner')}>量比</td>
+                            <td>板块</td>
                             <td>操作</td>
                         </tr>
                     </thead>
                     <tbody>
-                    {
-                        this.state.owner.map((item,index)=>{
-                            return (
-                                <Item action={this.action.bind(this,'owner')} hasOwner={true} key={item.code} index={index} data={item} onUpdate={this.onUpdate.bind(this,index,'owner')}/>
-                            )
-                        })
-                    }
+                        {
+                            this.state.owner.map((item, index) => {
+                                return (
+                                    <Item action={this.action.bind(this, 'owner')} hasOwner={true} key={item.code + 'o' + index} index={index} data={item} onUpdate={this.onUpdate.bind(this, index, 'owner')} />
+                                )
+                            })
+                        }
                     </tbody>
                 </table>
             </div>
--- src/pages/list/item.jsx
+++ src/pages/list/item.jsx
@@ -1,9 +1,9 @@
-import {PureComponent} from 'react';
-import {Jsonp} from 'utils/curry';
-export default class Item extends PureComponent{
+import { PureComponent } from 'react';
+import { Jsonp } from 'utils/curry';
+export default class Item extends PureComponent {
     timer = null;
-    id=0;
-    constructor(props){
+    id = 0;
+    constructor(props) {
         super(props);
         // <td>涨幅</td>
         // <td>实时价格</td>
@@ -24,9 +24,10 @@ export default class Item extends PureComponent{
             ltsz,
             hsl,
             sy,
-            lb
+            lb,
+            bk
         } = props.data;
-        this.state= {
+        this.state = {
             zf,
             jg,
             jk,
@@ -36,68 +37,100 @@ export default class Item extends PureComponent{
             ltsz,
             hsl,
             sy,
-            lb
+            lb,
+            bk
         }
     }
-    componentWillUnmount(){
-        if(this.timer){
+    componentWillUnmount() {
+        if (this.timer) {
             clearTimeout(this.timer);
             this.timer = null;
         }
     }
-    componentDidMount(){
+    componentDidMount() {
         this.start();
         this.requestInfo();
+
+        let id = '';
+        if (id == 7) {
+            id = (this.id.substr(5, 1) == "1" ? "1" : '0') + '.' + this.id.substr(0, 6)
+        } else {
+            id = (this.id.substr(0, 1) == "0" ? "0" : '1') + '.' + this.id.substr(0, 6)
+        }
+        if(localStorage.getItem(id)){
+            this.setState({ bk:localStorage.getItem(id) },()=>{
+                this.props.onUpdate(this.state);
+            })
+        }else{
+            Jsonp('http://push2.eastmoney.com/api/qt/slist/get?ut=fa5fd1943c7b386f172d6893dbfba10b&spt=3&pi=0&pz=5&po=1&fields=f14,f3,f128,f12,f13,f100,f102,f103&secid=' + id, {
+                param: 'cb',
+            }, res => {
+                console.log(res)
+                if (res.data) {
+                    let diff = res.data.diff;
+                    let arr = []
+                    for (let k in diff) {
+                        arr.push(diff[k])
+                    }
+                    let bk = arr.map(item => item.f14).join(';');
+                    localStorage.setItem(id,bk);
+                    this.setState({ bk },()=>{
+                        this.props.onUpdate(this.state);
+                    })
+                }
+            })
+        }
     }
-    start(){
-        this.timer = setTimeout(this.requestInfo.bind(this),5000)
+    start() {
+        // this.timer = setTimeout(this.requestInfo.bind(this),10000)
     }
-    requestInfo(){
-        Jsonp('//nuff.eastmoney.com/EM_Finance2015TradeInterface/JS.ashx?token=beb0a0047196124721f56b0f0ff5a27c&id='+this.id,{
-            param:'cb',
-        },res=>{
+    requestInfo() {
+        Jsonp('http://nuff.eastmoney.com/EM_Finance2015TradeInterface/JS.ashx?token=beb0a0047196124721f56b0f0ff5a27c&id=' + this.id, {
+            param: 'cb',
+        }, res => {
             // console.log(res)
             let arr = res.Value;
-            if(arr[29]){
+            if (arr[29]) {
                 let data = {
-                    zf:arr[29],
-                    jg:arr[8],
-                    jk:arr[28],
-                    zs:arr[34],
-                    zg:arr[30],
-                    zd:arr[32],
-                    ltsz:Number(arr[45]/10000/10000).toFixed(2),
-                    hsl:arr[37],
-                    sy:arr[38],
-                    lb:arr[36],
-                    name:arr[2]
+                    zf: arr[29],
+                    jg: arr[8],
+                    jk: arr[28],
+                    zs: arr[34],
+                    zg: arr[30],
+                    zd: arr[32],
+                    ltsz: Number(arr[45] / 10000 / 10000).toFixed(2),
+                    hsl: arr[37],
+                    sy: arr[38],
+                    lb: arr[36],
+                    name: arr[2]
                 };
-                this.setState(data);
-                this.props.onUpdate && this.props.onUpdate(data);
+                let olst = Object.assign(this.state, data);
+                this.setState(olst);
+                this.props.onUpdate && this.props.onUpdate(olst);
                 this.timer && this.start();
             }
         })
     }
-    Del = (code)=>{
-        this.props.action('del',this.id)
+    Del = (code) => {
+        this.props.action('del', this.id)
     }
-    addOwner = (code)=>{
-        this.props.action('addowner',this.id)
+    addOwner = (code) => {
+        this.props.action('addowner', this.id)
     }
-    DelOwner = (code)=>{
-        this.props.action('delowner',this.id)
+    DelOwner = (code) => {
+        this.props.action('delowner', this.id)
     }
-    render(){
-        let {data,index} =this.props;
-        let {code ,name,info} = data;
+    render() {
+        let { data, index } = this.props;
+        let { code, name, info } = data;
         this.id = code;
         return (
             <tr>
                 <td>{index}</td>
-                <td>{code}</td>
+                <td><a target="bank" href={"http://stockpage.10jqka.com.cn/HQ_v4.html?v=_yk_2222#hs_" + code.substr(0, 6)}>{code}</a></td>
                 <td>{name}</td>
                 <td>{info}</td>
-                <td className={this.state.zf>0?'red':'green'}>{this.state.zf}%</td>
+                <td className={this.state.zf > 0 ? 'red' : 'green'}>{this.state.zf}%</td>
                 <td>{this.state.jg}</td>
                 <td>{this.state.jk}</td>
                 <td>{this.state.zs}</td>
@@ -107,7 +140,8 @@ export default class Item extends PureComponent{
                 <td>{this.state.hsl}</td>
                 <td>{this.state.sy}</td>
                 <td>{this.state.lb}</td>
-                <td><nobr>{this.props.hasOwner?<span><button onClick={this.Del}>删除</button><button onClick={this.DelOwner}>-</button></span>:<span><button onClick={this.addOwner}>+</button><button onClick={this.Del}>删除</button></span>}</nobr></td>
+                <td>{this.state.bk}</td>
+                <td><nobr>{this.props.hasOwner ? <span><button onClick={this.Del}>删除</button><button onClick={this.DelOwner}>-</button></span> : <span><button onClick={this.addOwner}>+</button><button onClick={this.Del}>删除</button></span>}</nobr></td>
             </tr>
         )
     }
